b0b5341d90a0aebae930502a7b68b14a2cc55621
Anton Tolken <anton.tolken@icloud.com>
2024-11-19 18:55:50 +1300

    add missed backend contract for scan product

---
+++ backend/API/Entities/ScanProduct.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingCart.Entities;
+
+public class ScanProduct(string sku)
+{
+    // todo-at: should product SKU be case-sensitive? at the moment it is (case-sensitive)
+    public string Sku { get; set; } = sku;
+}
